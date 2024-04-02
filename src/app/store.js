c8c13fe7b672f1285c770fc9d1ab9aca02f92ed7
tolerevo <tolerevo@hotmail.com>
2024-04-02 03:26:58 -0600

    add TopicSlice

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import topicsReducer from "../features/topics/topicsSlice";
+// import reducers
+
+export default configureStore({
+  reducer: {
+    topics: topicsReducer
+  },
+});
